d89a4d0691bc808fd0f675db21a5a4da3ffe5cf4
Piyush-987 <piyushchaudharyy551@gmail.com>
2025-08-31 02:56:35 +0530

    created some panels in centre

--- campa/src/components/CenterPart.js
+++ campa/src/components/CenterPart.js
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import img1 from '../images/img1.jpg'
 function CenterPart() {
   return (
-    <div className='center'>CenterPart</div>
-  )
+    <>
+    <div className='center'>
+    <div className="heading">Jharkahnd</div>
+    <img style={{marginBottom:'20px',marginTop:'20px',paddingLeft:'35px',width:'950px', height:'300px'}} src={img1}></img>
+    <h4>Forest Intelligence</h4>
+    <div class="d-flex justify-content-around">
+        <div className='new1'>yo</div>
+        <div className='new1'>yo</div>
+        <div className='new1'>yo</div>
+        <div className='new1'>yo</div>
+    </div>
+    <h4>Real Time</h4>
+    <div class="d-flex justify-content-around">
+        <div className='new'>yo</div>
+        <div className='new'>yo</div>
+    </div>
+    <div class="d-flex justify-content-around">
+        <div className='new'>yo</div>
+        <div className='new'>yo</div>
+    </div>
+    <div class="d-flex justify-content-around">
+        <div className='new2'>yo</div>
+    </div>
+    <h4>Real Time</h4>
+    <div class="d-flex justify-content-around">
+        <div className='new3'>yo</div>
+        <div className='new3'>yo</div>
+        <div className='new3'>yo</div>
+    </div>
+    </div>
+  </>)
 }
 
 export default CenterPart
